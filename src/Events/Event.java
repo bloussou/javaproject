95dbf8326d49ef17ba157cb11d5e6b67d92a57bd
Ludo171 <ludovic.descateaux@student.ecp.fr>
2017-11-20 18:25:30 +0100

    Début implémentation Events & EventsManager

---
+++ src/Events/Event.java
@@ -0,0 +1,28 @@
+package Events;
+
+public abstract class Event {
+
+	private TimeStamp startTime;
+	private TimeStamp endTime;
+	
+	
+	
+	
+	
+	public TimeStamp getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(TimeStamp startTime) {
+		this.startTime = startTime;
+	}
+	public TimeStamp getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(TimeStamp endTime) {
+		this.endTime = endTime;
+	}
+	
+	
+	
+	
+}
---
+++ src/Events/EventsManager.java
@@ -0,0 +1,46 @@
+package Events;
+
+import java.util.ArrayList;
+
+import Emergency.ED;
+
+public class EventsManager {
+
+	public ArrayList<Event> inProgress;
+	public ArrayList<ED> eds;
+	
+	public EventsManager() {
+		this.inProgress = new ArrayList<Event>();
+		this.eds = new ArrayList<ED>();
+	}
+	
+	
+	public void checkNewEvents(){
+		
+	}
+	
+	private void checkNewRegistration(){
+		for (ED ed : eds) {
+		}
+		
+		
+	}
+	
+	public void timeGoesToNextEventEnd(){
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		EventsManager EM = new EventsManager();
+		ED hosp1 = new ED("Hosp1", "France");
+		ED hosp2 = new ED("Hosp2", "France");
+		
+		EM.eds.add(hosp2);
+		EM.eds.add(hosp1);
+		
+		EM.checkNewRegistration();
+				
+	}
+	
+}
