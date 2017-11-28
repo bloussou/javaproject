7d9f2463324a6d6379ee3c7662417f4070438fef
bloussou <brieuc.loussouarn@student.ecp.fr>
2017-11-28 23:36:56 +0100

    ajout de qq trucs

---
+++ src/Test/WaitingRoomTest.java
@@ -0,0 +1,39 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WaitingRoomTest {
+
+	@Test
+	public void testAddOccupant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveOccupant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePatientCharge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitingRoomEDString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitingRoomED() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOccupants() {
+		fail("Not yet implemented");
+	}
+
+}
