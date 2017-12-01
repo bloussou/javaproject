3f74cb226453fb1362566ed6179cdd39eb62a93f
bloussou <brieuc.loussouarn@student.ecp.fr>
2017-12-01 19:48:19 +0100

    messages

--- src/MessageBox/Message.java
+++ src/MessageBox/Message.java
@@ -45,6 +45,7 @@ public class Message {
 	}
 	
 	
+	
 
 
 	public int getId() {
--- src/Test/WaitingRoomTest.java
+++ src/Test/WaitingRoomTest.java
@@ -4,11 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Emergency.ED;
+import Events.Time;
+import Events.TimeStamp;
+import HR.Patient;
+import HR.Physician;
+import Rooms.WaitingRoom;
+
 public class WaitingRoomTest {
 
 	@Test
 	public void testAddOccupant() {
-		fail("Not yet implemented");
+		ED ed = new ED("ed1","france");
+		Time time = Time.getInstanceTime();
+		time.timeGoes(10);
+		WaitingRoom waiting1 = new WaitingRoom(ed);
+		Patient patient1 = new Patient(ed,"L1",new TimeStamp());
+		Patient patient2 = new Patient(ed,"L2", new TimeStamp());
+		waiting1.addOccupant(patient);
+		assertTrue(waiting1.getOccupants().equals([patient]));
 	}
 
 	@Test
@@ -31,9 +45,6 @@ public class WaitingRoomTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testGetOccupants() {
-		fail("Not yet implemented");
-	}
+
 
 }
