95191b1e0cc13bfa25de6b3f665e063bb6ca99bd
bloussou <brieuc.loussouarn@student.ecp.fr>
2017-12-03 19:50:44 +0100

    java doc de ed terminée

--- src/Emergency/ED.java
+++ src/Emergency/ED.java
@@ -64,21 +64,105 @@ public class ED {
 	 */
 	private ArrayList<ArrayList<Physician>> dbPhysician; 
 	/**
+	 * La liste des transporteurs de l'hôpital et de leur état.
+	 * Elle est composées de trois sous listes sous cette forme :
+	 * [[idle],[transportation],[ofDuty]]
 	 * 
+	 * @see ED#getDbTransporter() 
 	 */
-	private ArrayList<ArrayList<Transporter>> dbTransporter; // [[idle],[transportation],[ofDuty]]
-	private ArrayList<ArrayList<Patient>> dbPatient; // [[arrived],[registered],[transporting],[waitingForConsultation],[inConsultation],[waitingForMRI],[waitingForBloodTest],[waitingForRadio],[transportation],[waitingForMRIT],[waitingForBloodTestT],[waitingForRadioT],[bloodTested],[mriTested],[radioTested],[released]]
-	private ArrayList<ArrayList<Stretcher>> dbStretcher; // [[free],[occupied]]
-	private ArrayList<ArrayList<WaitingRoom>> dbWaitingRoom; // [[available],[full]]
-	private ArrayList<ArrayList<BoxRoom>> dbBoxRoom; // [[free],[occupied]]
-	private ArrayList<ArrayList<ShockRoom>> dbShockRoom; // [[free],[occupied]]
-	private ArrayList<ArrayList<BloodRoom>> dbBloodRoom; // [[free],[occupied]]
-	private ArrayList<ArrayList<MRIRoom>> dbMRIRoom; // [[free],[occupied]]
-	private ArrayList<ArrayList<RadioRoom>> dbRadioRoom; // [[free],[occupied]]
+	private ArrayList<ArrayList<Transporter>> dbTransporter; 
+	/**
+	 * La liste des patients de l'hôpital et de leur état.
+	 * Elle est composées de 15 sous listes sous cette forme :
+	 * [[arrived],[registered],[transporting],[waitingForConsultation],[inConsultation],[waitingForMRI],[waitingForBloodTest],[waitingForRadio],[transportation],[waitingForMRIT],[waitingForBloodTestT],[waitingForRadioT],[bloodTested],[mriTested],[radioTested],[released]]
+	 * 
+	 * @see ED#getDbPatient() 
+	 */
+	private ArrayList<ArrayList<Patient>> dbPatient; 
+	/**
+	 * La liste des stretchers de l'hôpital et de leur état.
+	 * Elle est composées de deux sous listes sous cette forme :
+	 * [[free],[occupied]]
+	 * 
+	 * @see ED#getDbStretcher() 
+	 */
+	private ArrayList<ArrayList<Stretcher>> dbStretcher; 
+	/**
+	 * La liste des waiting rooms de l'hôpital et de leur état.
+	 * Elle est composées de deux sous listes sous cette forme :
+	 * [[available],[full]]
+	 * 
+	 * @see ED#getDbWaitingRoom() 
+	 */
+	private ArrayList<ArrayList<WaitingRoom>> dbWaitingRoom; 
+	/**
+	 * La liste des box rooms de l'hôpital et de leur état.
+	 * Elle est composées de deux sous listes sous cette forme :
+	 * [[free],[occupied]]
+	 * 
+	 * @see ED#getDbBoxRoom()
+	 */
+	private ArrayList<ArrayList<BoxRoom>> dbBoxRoom; 
+	/**
+	 * La liste des shockrooms de l'hôpital et de leur état.
+	 * Elle est composées de deux sous listes sous cette forme :
+	 * [[free],[occupied]]
+	 * 
+	 * @see ED#getDbShockRoom() 
+	 */
+	private ArrayList<ArrayList<ShockRoom>> dbShockRoom; 
+	/**
+	 * La liste des bloodrooms de l'hôpital et de leur état.
+	 * Elle est composées de deux sous listes sous cette forme :
+	 * [[free],[occupied]]
+	 * 
+	 * @see ED#getDbBloodRoom() 
+	 */
+	private ArrayList<ArrayList<BloodRoom>> dbBloodRoom; 
+	/**
+	 * La liste des mriroom de l'hôpital et de leur état.
+	 * Elle est composées de deux sous listes sous cette forme :
+	 * [[free],[occupied]]
+	 * 
+	 * @see ED#getDbMRIRoom() 
+	 */
+	private ArrayList<ArrayList<MRIRoom>> dbMRIRoom; 
+	/**
+	 * La liste des radioroom de l'hôpital et de leur état.
+	 * Elle est composées de deux sous listes sous cette forme :
+	 * [[free],[occupied]]
+	 * 
+	 * @see ED#getDbRadioRoom() 
+	 */
+	private ArrayList<ArrayList<RadioRoom>> dbRadioRoom; 
 
 	
 	
-	
+	/**
+	 * Constructor of the ed taking two parameters. It sets all the db ofthe ED and give him a unique ID
+	 * @param name
+	 * 			the name of the ed
+	 * @param country
+	 * 			the country of the ED
+	 * 
+	 * @see ED#country
+	 * @see ED#id
+	 * @see ED#name
+	 * @see ED#dbNurse
+	 * @see ED#dbBloodRoom
+	 * @see ED#dbBoxRoom
+	 * @see ED#dbPhysician
+	 * @see ED#dbTransporter
+	 * @see ED#dbPatient
+	 * @see ED#dbStretcher
+	 * @see ED#dbWaitingRoom
+	 * @see ED#dbShockRoom
+	 * @see ED#dbBloodRoom
+	 * @see ED#dbMRIRoom
+	 * @see ED#dbRadioRoom
+	 * @see ED#edRegister
+	 * 
+	 */
 	
 	
 	public ED(String name, String country){
@@ -167,8 +251,7 @@ public class ED {
 	}
 	
 	/*
-	 * update Nurse after a state changement of the nurse
-	 */
+
 
 	public void updateNurse(Nurse nurse) {
 		if(nurse.getState().equals("idle") && this.dbNurse.get(1).contains(nurse) ){
@@ -196,7 +279,7 @@ public class ED {
 			this.dbNurse.get(2).add(nurse);
 		}
 	}
-	
+	*/
 
 
 
@@ -209,7 +292,11 @@ public class ED {
 
 
 
-
+	/**
+	 * @return 
+	 * 		{@link ED#dbPhysician}
+	 * 
+	 */
 
 	public ArrayList<ArrayList<Physician>> getDbPhysician() {
 		return dbPhysician;
@@ -218,7 +305,12 @@ public class ED {
 
 
 
-
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#dbTransporter}
+	 * 
+	 */
 
 	public ArrayList<ArrayList<Transporter>> getDbTransporter() {
 		return dbTransporter;
@@ -228,7 +320,12 @@ public class ED {
 
 
 
-
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#dbPatient}
+	 * 
+	 */
 	public ArrayList<ArrayList<Patient>> getDbPatient() {
 		return dbPatient;
 	}
@@ -236,7 +333,11 @@ public class ED {
 
 
 
-
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#dbStretcher}
+	 */
 
 	public ArrayList<ArrayList<Stretcher>> getDbStretcher() {
 		return dbStretcher;
@@ -245,7 +346,11 @@ public class ED {
 
 
 
-
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#dbWaitingRoom}
+	 */
 
 	public ArrayList<ArrayList<WaitingRoom>> getDbWaitingRoom() {
 		return dbWaitingRoom;
@@ -254,7 +359,12 @@ public class ED {
 
 
 
-
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#dbBoxRoom}
+	 
+	 */
 
 	public ArrayList<ArrayList<BoxRoom>> getDbBoxRoom() {
 		return dbBoxRoom;
@@ -263,7 +373,11 @@ public class ED {
 
 
 
-
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#dbBloodRoom}
+	 */
 
 	public ArrayList<ArrayList<BloodRoom>> getDbBloodRoom() {
 		return dbBloodRoom;
@@ -272,15 +386,22 @@ public class ED {
 
 
 
-
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#dbMRIRoom}
+	 */
 
 	public ArrayList<ArrayList<MRIRoom>> getDbMRIRoom() {
 		return dbMRIRoom;
 	}
 
 
-
-
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#dbRadioRoom}
+	 */
 
 
 	public ArrayList<ArrayList<RadioRoom>> getDbRadioRoom() {
@@ -290,7 +411,11 @@ public class ED {
 
 
 
-
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#dbShockRoom}
+	 */
 
 	public ArrayList<ArrayList<ShockRoom>> getDbShockRoom() {
 		return dbShockRoom;
@@ -299,38 +424,82 @@ public class ED {
 
 
 
-
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#compteurEDId}
+	 */
 
 	public static int getCompteurEDId() {
 		return compteurEDId;
 	}
+	/**
+	 * 
+	 * @return
+	 * 		{@link ED#name}
+	 */
 	public String getName() {
 		return name;
 	}
+	/**
+	 * Set the name of the ED
+	 * @param name
+	 * @see ED#name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**
+	 * 
+	 * @return {@link ED#country}
+	 */
 	public String getCountry() {
 		return country;
 	}
+	/**
+	 * Set the country of the ED
+	 * @param country
+	 * @see ED#country
+	 */
 	public void setCountry(String country) {
 		this.country = country;
 	}
+	/**
+	 * Set the id of the ED
+	 * @param id
+	 * @see ED#id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	/**
+	 * 
+	 * @return {@link ED#id}
+	 */
 	public int getId() {
 		return id;
 	}
-
+	/**
+	 * 
+	 * @return {@link ED#edRegister}
+	 */
 	public ArrayList<Patient> getEdRegister() {
 		return edRegister;
 	}
-
+	/**
+	 * Add a patient to the list edRgister
+	 * @param patient
+	 * 
+	 */
 	public void addPatientToEdRegister(Patient patient) {
 		this.edRegister.add(patient);
 	}
 		
+	
+	/**
+	 * remove a patient from the list edRegister
+	 * @param patient
+	 */
 	public void removePatientFromEdRegister(Patient patient) {
 		this.edRegister.remove(patient);
 	}
