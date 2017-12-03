f76fae3687010461dadc9668156daf11ddddb282
bloussou <brieuc.loussouarn@student.ecp.fr>
2017-12-04 00:06:47 +0100

    jdk de message et de toutes les probas terminées

--- src/Proba/Gamma.java
+++ src/Proba/Gamma.java
@@ -8,7 +8,12 @@ public class Gamma  {
 	
 	public Gamma(){}
 
-	
+	/**
+	 * Return a sample which follows a gamma distribution with the parameters k and theta
+	 * @param k
+	 * @param theta
+	 * @return {@link GammaDistribution#sample()}
+	 */
 	public double randSample(double k, double theta) {
 		return new GammaDistribution(k, theta).sample();
 	}
